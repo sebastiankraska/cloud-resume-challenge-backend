2f8e2c5d65d953bff93d99cc379fda7a946bc940
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-11-02 14:22:07 +0100

    initial terraform config and gitignore

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "crc-terraform-state-sk"
+        key = "terraform.tfstate"
+        region = "eu-central-1"
+        encrypt = true
+        use_lockfile = true
+    }
+}
